c94af10327d96912d7ed7841b55589785c4d0734
zaidabdillah18 <zaidabdillah18@gmail.com>
2022-12-02 20:30:24 +0700

    akun login register, home, program, kegiatan  dan data penyandang

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ const kegiatanpenyandangRouter = require('./routes/kegiatanpenyandang')
 const corsConfig= {
   credentials:true,
 }
-app.use(cors(corsConfig))
-app.options("*",cors(corsConfig))
-app.use(function(req,res, next){
-  res.header("Access-Control-Allow-Credentials","true");
-  next();
-})
+app.use(cors())
+// app.options("*",cors(corsConfig))
+// app.use(function(req,res, next){
+//   res.header("Access-Control-Allow-Credentials","true");
+//   next();
+// })
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
